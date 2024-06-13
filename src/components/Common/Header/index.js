b8d743375ad4c54747880e65abb55a040e95f4f1
Anil <anil6996f@gmail.com>
2024-06-13 18:36:58 +0530

    Implemented a drawer using MUI for mobile screens

--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import './styles.css';
+import AnchorTemporaryDrawer from './drawer';
 
 function Header() {
   return (
-    <div className='header'>Header</div>
+    <div className='navbar'>
+          <h1 className='logo'>CryptoTracker<span style={{ color: 'var(--blue)' }}>.</span></h1>
+          <div className='links'>
+              <a href="/">
+                  <p className='link'>Home</p>
+              </a>
+              <a href="/">
+                  <p className='link'>Compare</p>
+              </a>
+              <a href="/">
+                  <p className='link'>Watchlist</p>
+              </a>
+              <a href="/">
+                  <p className='link'>Dashboard</p>
+              </a>
+          </div>
+          <div className='mobile-drawer'>
+              <AnchorTemporaryDrawer/>
+          </div>
+    </div>
   )
 }
 
