f75fa904826d06aa2c6d77a80620ea9bef26a68c
Anil <anil6996f@gmail.com>
2024-06-16 14:16:16 +0530

    Completed the pagination functionality

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,10 +3,19 @@ import Header from '../components/Common/Header';
 import TabsComponent from '../components/Dashboard/Tabs';
 import axios from 'axios';
 import Search from '../components/Dashboard/Search';
+import  PaginationComponent  from '../components/Dashboard/Pagination';
 
 function Dashboard () {
   const [coins, setCoins] = useState([]);
+  const [paginatedCoins,setPaginatedCoins] = useState([])
   const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+
+  const handlePageChange = (event, value) => {
+    setPage(value)
+    let previousIndex = (value - 1) * 10;
+    setPaginatedCoins(coins.slice(previousIndex, previousIndex + 10));
+  }
 
   const onSearchChange = (e) => {
     setSearch(e.target.value);
@@ -19,6 +28,7 @@ function Dashboard () {
       try {
         const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
         setCoins((response.data));
+        setPaginatedCoins(response.data.slice(0, 10));
       } catch (error) {
         console.log(error);
       }
@@ -30,7 +40,8 @@ function Dashboard () {
     <div>
       <Header />  
       <Search search={search} onSearchChange={onSearchChange}/>
-      <TabsComponent coins={filteredCoins} />
+      <TabsComponent coins={search ? filteredCoins : paginatedCoins} />
+      {!search && <PaginationComponent page={page} handlePageChange={handlePageChange}/>}
     </div>
   )
 };
